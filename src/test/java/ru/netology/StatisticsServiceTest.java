31dfb3ebeeea120e2301891bbd31012f0510b856
Jane <evgenia.kochukova@gmail.com>
2023-03-01 00:03:20 -0300

    Second commit

---
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxLow() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
